cb776fbdff5e53d26f64dc388edd5afb7a404fc5
Fanny Rubiño <fannyrubino@gmail.com>
2020-10-08 17:29:53 +0000

    Update backend.tf

--- src/backend.tf
+++ src/backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    region         = "eu-west-3"
-    bucket         = "tfst"
-    key            = "/azure/tfst.tfstate"
-    role_arn       = "arn:aws:iam::terraform:role/deploy"
+  backend "http" {
+
   }
 }
