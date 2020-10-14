6571b7d3eb353fcb6d49b5f61f9d861d9ff7cc1b
Fanny Rubiño <fannyrubino@gmail.com>
2020-10-14 19:06:35 +0000

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -2,8 +2,8 @@
 
 variable "ARM_CLIENT_ID" {}
 variable "ARM_CLIENT_SECRET" {}
-variable "ARM_SUBSCRIPTION_ID" {}
-variable "ARM_TENANT_ID" {}
+#variable "ARM_SUBSCRIPTION_ID" {}
+#variable "ARM_TENANT_ID" {}
 
 terraform {
   required_providers {
@@ -11,10 +11,10 @@ terraform {
       	source = "hashicorp/azurerm"
       	version = ">= 1.37.0"
 		
-		subscription_id = var.ARM_SUBSCRIPTION_ID
-  		client_id       = var.ARM_CLIENT_ID
-  		client_secret   = var.ARM_CLIENT_SECRET
-  		tenant_id       = var.ARM_TENANT_ID
+		#subscription_id = var.ARM_SUBSCRIPTION_ID
+  		#client_id       = var.ARM_CLIENT_ID
+  		#client_secret   = var.ARM_CLIENT_SECRET
+  		#tenant_id       = var.ARM_TENANT_ID
 
     }
     azuread = {
